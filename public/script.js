8efb638871562ba1ee26dc7d68ca234baf9b9a87
hrupesh <rcashok0@gmail.com>
2020-08-20 21:19:44 +0530

    Auto Commit 😎

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 const socket = io("/");
+const myPeer = new myPeer(undefined, {
+  host: "/",
+  post: "3001",
+});
 
 socket.emit("join-room", ROOM_ID, 4);
 
 socket.on("user-connected", (userId) => {
-  console.log("Welcome  "+userId);
+  console.log("Welcome  " + userId);
 });
