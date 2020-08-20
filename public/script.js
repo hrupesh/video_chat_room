1bd8c8506bb1697c0f73177618ed3ebb49871d42
hrupesh <rcashok0@gmail.com>
2020-08-20 23:58:27 +0530

    Auto Commit 😎

--- public/script.js
+++ public/script.js
@@ -18,11 +18,15 @@ navigator.mediaDevices
 
     myPeer.on("call", (call) => {
       call.answer(stream);
+      //   alert("New User Connected!");
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
     });
 
     socket.on("user-connected", (userId) => {
       connectToNewUser(userId, stream);
-    //   alert("New User Connected!");
     });
   });
 
