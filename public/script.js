3fc20c32a422e6fe791f8e303ec801e0a5cc5155
hrupesh <rcashok0@gmail.com>
2020-08-21 00:06:37 +0530

    Auto Commit 😎

--- public/script.js
+++ public/script.js
@@ -30,6 +30,10 @@ navigator.mediaDevices
     });
   });
 
+socket.on("user-disconnected", (userId) => {
+  console.log("Disconnected:  " + userId);
+});
+
 myPeer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
 });
